f109f8f5a987593bfea6c1db072f1eb31ee54c29
Yegor249 <egor.nedov11@gmail.com>
2024-04-07 20:21:30 +0300

    Homework lesson 5

--- SwiftLesson5.playground/Contents.swift
+++ SwiftLesson5.playground/Contents.swift
@@ -1,22 +1,46 @@
-//Lesson 5
-// Basic operators
-/*
-var small : UInt8 = 255
-small = small &+ 1
-print(small)
-*/
+//  Lesson 5
+//  Basic operators
+//  Home Wor
+// 1) Используя арифметичиские уравнения посичтать количество секунд от начала года до  моего ДР (11.09)
+// 2) Вывести на екран квартал в котором ты родился
+// 3) Высчитать какого цвета квадрат на шахмотной доске иммея заданные параметры столбца и ряда
 
+let amountDaysInYear   = 365
+let amountDaysInMonth  = ["January": 31, "February": 28, "March": 31, "April": 30, "May": 31, "June": 30, "July": 31, "August": 31, "September": 30, "October": 31, "November": 30, "December": 31]
+let amountSecondsInDay = 86_400
+let birthday           = (month: 10, day: 11)
 
-let a = 5
-let b = 6
-var c: Int
+var temp    = 0     // Временная переменная для цикла
+var daysSum = 0
 
-if a < b {
-    c = a
-} else {
-    c = b
+for (_, amountDays) in amountDaysInMonth{
+    temp += 1
+    if temp == birthday.month {
+        print("Количество секунд до твоего дня рождения:\(amountSecondsInDay * (daysSum + birthday.day))")
+        
+        if Double(temp) / 3 <= 1 {
+            print("Ты родился в 1 квартале")
+        } else if Double(temp) / 3 > 1 && Double(temp) / 3  <= 2 {
+            print("Ты родился в 2 квартале")
+        } else if Double(temp) / 3 > 2 && Double(temp) / 3  <= 3 {
+            print("Ты родился в 3 квартале")
+        } else if Double(temp) / 3 > 3 && Double(temp) / 3  <= 4 {
+            print("Ты родился в 4 квартале")
+        }
+        
+    } else{
+        daysSum += amountDays
+    }
 }
 
 
-c =  (a < b) ? a : b
+// Шахмотная доска
 
+let row    = 2
+let column = 6
+
+if row % 2 == 0 && column % 2 == 0 || row % 2 != 0 && column % 2 != 0 {
+    print("Квадрат чёрный")
+} else {
+    print("Квадрат белый")
+}
---
+++ SwiftLesson6.playground/Contents.swift
@@ -0,0 +1,2 @@
+// Swift Lesson 6
+
